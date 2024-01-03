ae0b994531417350ce3a18a6005d427d5dc78ae0
FlorianClaisse <floc.claisse@gmail.com>
2024-01-03 18:34:41 +0100

    Fix endpoint

--- src/main/kotlin/org/labeli/serversdk/endpoint/AuthResource.kt
+++ src/main/kotlin/org/labeli/serversdk/endpoint/AuthResource.kt
@@ -1,111 +1,58 @@
 package org.labeli.serversdk.endpoint
 
-import okhttp3.Request
-import org.labeli.serversdk.HTTPMediaType.Application.Companion.json
-import org.labeli.serversdk.HTTPMediaType.Companion.application
+import org.labeli.serversdk.HTTPMethod
+import org.labeli.serversdk.HTTPRequest
 import org.labeli.serversdk.dto.authentication.request.ClientRegisterRequest
 import org.labeli.serversdk.dto.authentication.request.LoginRequest
 import org.labeli.serversdk.dto.authentication.request.RefreshTokenRequest
 import org.labeli.serversdk.dto.authentication.request.UserRegisterRequest
+import org.labeli.serversdk.dto.authentication.response.*
 import org.labeli.serversdk.extensions.div
-import org.labeli.serversdk.extensions.patch
-import org.labeli.serversdk.extensions.post
 
 internal class AuthResource(path: String) {
     internal val user = UserResource(path / "user")
     internal val client = ClientResource(path / "client")
 
     internal inner class UserResource(private val path: String) {
-        internal val getAllAdmin = Request.Builder()
-                                          .url(path / "admin" / "all")
-                                          .get()
-                                          .build()
+        internal val getAllAdmin = HTTPRequest<List<UserPrivateResponse>>(path / "admin" / "all")
+        internal val getAllSeller = HTTPRequest<List<UserPrivateResponse>>(path / "seller" / "all")
+        internal val deleteMe = HTTPRequest<Unit>(path / "delete", HTTPMethod.DELETE)
 
-        internal val getAllSeller = Request.Builder()
-                                           .url(path / "seller" / "all")
-                                           .get()
-                                           .build()
+        internal fun login(user: LoginRequest): HTTPRequest<UserLoginResponse> =
+            HTTPRequest(path / "login", HTTPMethod.POST, body = user)
 
-        internal val deleteMe = Request.Builder()
-                                       .url(path / "delete")
-                                       .delete()
-                                       .build()
+        internal fun refreshToken(token: RefreshTokenRequest): HTTPRequest<TokenResponse> =
+            HTTPRequest(path / "refreshTokens", HTTPMethod.POST, body = token)
 
-        internal fun login(user: LoginRequest): Request =
-            Request.Builder()
-                   .url(path / "login")
-                   .post(user, application(json))
-                   .build()
+        internal fun register(user: UserRegisterRequest): HTTPRequest<Unit> =
+            HTTPRequest(path / "register", HTTPMethod.POST, body = user)
 
-        internal fun refreshToken(token: RefreshTokenRequest): Request =
-            Request.Builder()
-                   .url(path / "refreshTokens")
-                   .post(token, application(json))
-                   .build()
+        internal fun updateIsAdmin(isAdmin: Boolean, email: String): HTTPRequest<Unit> =
+            HTTPRequest(path / "update" / "isAdmin" / email / isAdmin.toString(), HTTPMethod.PATCH)
 
-        internal fun register(user: UserRegisterRequest): Request =
-            Request.Builder()
-                   .url(path / "register")
-                   .post(user, application(json))
-                   .build()
 
-        internal fun updateIsAdmin(isAdmin: Boolean, email: String): Request =
-            Request.Builder()
-                   .url(path / "update" / "isAdmin" / email / isAdmin.toString())
-                   .patch()
-                   .build()
+        internal fun updateCanSell(canSell: Boolean, email: String): HTTPRequest<Unit> =
+            HTTPRequest(path / "update" / "canSell" / email / canSell.toString(), HTTPMethod.PATCH)
 
-        internal fun updateCanSell(canSell: Boolean, email: String): Request =
-            Request.Builder()
-                   .url(path / "update" / "canSell" / email / canSell.toString())
-                   .patch()
-                   .build()
-
-        internal fun deleteByEmail(email: String): Request =
-            Request.Builder()
-                   .url(path / "delete" / email)
-                   .delete()
-                   .build()
+        internal fun deleteByEmail(email: String): HTTPRequest<Unit> =
+            HTTPRequest(path / "delete" / email, HTTPMethod.DELETE)
     }
 
     internal inner class ClientResource(private val path: String) {
-        internal val getAllMember = Request.Builder()
-                                           .url(path / "member" / "all")
-                                           .get()
-                                           .build()
+        internal val getAllMember = HTTPRequest<ClientPrivateResponse>(path / "member" / "all")
+        internal val resetMember = HTTPRequest<Void>(path / "reset" / "member", HTTPMethod.PATCH)
+        internal val deleteMe = HTTPRequest<Void>(path / "delete", HTTPMethod.DELETE)
 
-        internal val resetMember = Request.Builder()
-                                          .url(path / "reset" / "member")
-                                          .patch()
-                                          .build()
+        internal fun register(client: ClientRegisterRequest): HTTPRequest<Unit> =
+            HTTPRequest(path / "register", HTTPMethod.POST, body = client)
 
-        internal val deleteMe = Request.Builder()
-                                       .url(path / "delete")
-                                       .delete()
-                                       .build()
+        internal fun login(client: LoginRequest): HTTPRequest<ClientLoginResponse> =
+            HTTPRequest(path / "login", HTTPMethod.POST, body = client)
 
-        internal fun register(client: ClientRegisterRequest): Request =
-            Request.Builder()
-                   .url(path / "register")
-                   .post(client, application(json))
-                   .build()
+        internal fun refreshToken(token: RefreshTokenRequest): HTTPRequest<TokenResponse> =
+            HTTPRequest(path / "refreshTokens", HTTPMethod.POST, body = token)
 
-        internal fun login(client: LoginRequest): Request =
-            Request.Builder()
-                   .url(path / "login")
-                   .post(client, application(json))
-                   .build()
-
-        internal fun refreshToken(token: RefreshTokenRequest): Request =
-            Request.Builder()
-                   .url(path / "refreshTokens")
-                   .post(token, application(json))
-                   .build()
-
-        internal fun updateIsMember(isMember: Boolean, email: String): Request =
-            Request.Builder()
-                   .url(path / "update" / "isMember" / email / isMember.toString())
-                   .patch()
-                   .build()
+        internal fun updateIsMember(isMember: Boolean, email: String): HTTPRequest<Unit> =
+            HTTPRequest(path / "update" / "isMember" / email / isMember.toString(), HTTPMethod.PATCH)
     }
 }
